0c31383b6bfe6a8635741ee2753ad904d9684cd8
ananas99 <s94santos@gmail.com>
2018-11-20 12:36:22 +0000

    cnt increment changes

--- probC.ml
+++ probC.ml
@@ -13,13 +13,13 @@ type suffix_tree =
     |Empty
     |Node of int * suffix_tree array;;
 
-let createNode () = Node (1,Array.make 26 Empty);;
+let createNode n = Node (n,Array.make 26 Empty);;
 let getIndex c = (code c) - (code 'a');;
 
 
 
 
-let rec build_tree l root = match l with
+let rec build_tree l root nstring = match l with
     |[] -> ()
     |el::li -> 
         let rec aux n st = match st with
@@ -27,8 +27,13 @@ let rec build_tree l root = match l with
             |Node (_, vec) -> if  (n < length el) then
         	                    let index = getIndex (el.[n]) in
         	                    match (vec.(index)) with 
-                                    |Empty -> let () = vec.(index) <- createNode () in aux (n+1) vec.(index)
-    		                        |Node (_,_) -> aux (n+1) vec.(index) 
-    	                      else build_tree li root 	
+                                    |Empty -> let () = vec.(index) <- createNode 0 in aux (n+1) vec.(index)
+    		                        |Node (cnt,vec) -> if nstring < (cnt+1) then
+                                                         build_tree li root nstring    
+                                                     else 
+                                                        if nstring = (cnt+1) then 
+                                                            let () = vec.(index) <- Node((cnt+1),vec) in
+                                                            aux (n+1) vec.(index) 
+    	                      else build_tree li root nstring	
         in (aux 0 root)     	         
 ;;
