9a9b7395e17510ff40d380d44b21311385a784ca
ananas99 <s94santos@gmail.com>
2018-11-24 17:58:36 +0000

    search almost there

--- probC.ml
+++ probC.ml
@@ -1,5 +1,6 @@
-open String;;
-open Char;;
+type suffix_tree = 
+    |Empty
+    |Node of int * suffix_tree array;;
 
 
 let sublist s =
@@ -8,26 +9,21 @@ let sublist s =
     in (aux (String.length s) []) ;;
 
 
-type suffix_tree = 
-    |Empty
-    |Node of int * suffix_tree array;;
+let createNode () = Node (1,Array.make 26 Empty);;
 
-
-let createNode () = Node (0,Array.make 26 Empty);;
-let getIndex c = (code c) - (code 'a');;
-
+let getIndex c = (Char.code c) - (Char.code 'a');;
 
 let rec build_tree l root nstring = match l with
     |[] -> ()
     |el::li -> 
         let rec aux n st = match st with
             |Empty -> ()
-            |Node (_, vec) -> if  (n < length el) then
+            |Node (_, vec) -> if  (n < String.length el) then
                                 let index = getIndex (el.[n]) in
                                 match (vec.(index)) with 
                                     |Empty -> let () = vec.(index) <- createNode () in aux (n+1) vec.(index) 
                                     |Node (a,b) -> if (nstring < (a+1)) then 
-                                                       build_tree li root nstring  
+                                                       aux (n+1) vec.(index) 
                                                    else (if (nstring = (a+1)) then 
                                                             let () = vec.(index) <- Node(a+1,b) in
                                                             aux (n+1) vec.(index)
@@ -36,16 +32,48 @@ let rec build_tree l root nstring = match l with
         in (aux 0 root)                  
 ;; 
 
-let r = createNode()
-
-
-
-let rec abc l cnt = match l with
+let rec string_iter l cnt = match l with
     |[] -> ()
     |el::li -> let () = build_tree el r cnt in 
-               abc li (cnt+1)
+               string_iter li (cnt+1)
 ;;
 
-(*let () = build_tree ["banana";"anana";"nana";"ana";"na";"a"] r 1 *)
 
-let () = abc ["lll";"ll";"l"];["ab";"b"];["ll";"l"] 1
+let rec depth  nstr node acc l = match node with 
+                        |Empty -> () 
+                        |Node (a,b) -> Array.iter (fun x -> match x with 
+                                            |Empty -> ()                 
+                                            |Node (w,z) ->  if w = nstr then (
+                                                                let () = Printf.printf "%d\n" acc in
+                                                                depth nstr (Node(w,z)) (acc+1) (acc::l) )) b      
+                                                                 
+                                                           
+
+;;
+
+
+
+
+
+
+
+(*let () = build_tree ["banana";"anana";"nana";"ana";"na";"a"] r 1 *);;
+
+(*let () = string_iter [["lllaa";"llaa";"laa";"aa";"a"];["llaa";"laa";"aa";"a"]] 1;;*)
+
+let r = createNode();;
+let () = string_iter [["lll";"ll";"l"];["ll";"l"]] 1;;
+
+
+
+let l = [];;
+
+let () = depth 2 r 1 l;;
+
+(*let () = Printf.printf "%d\n" !z*);;
+
+
+
+
+
+let () = List.iter (Printf.printf "%d ") l
